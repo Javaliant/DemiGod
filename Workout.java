59b2216d3e9fbd183983fb05d728470c1a662edc
Luigi Vincent <luigivincenthr@gmail.com>
2014-06-25 02:04:51 -0400

    Update Workout.java

--- Workout.java
+++ Workout.java
@@ -12,57 +12,59 @@ import java.util.*;
 public class Workout extends JFrame {
 
 	public Workout() {
-
 		setLayout(new GridLayout(5, 3));
-
 		// Labels
 		add(new JLabel("Workout"));
 		add(new JLabel("Sets"));
 		add(new JLabel("Reps"));
+	}
 
+	public static void main(String[] args) throws Exception {
+		// Get current date information
+		Calendar cal = Calendar.getInstance();
+		// Set format to month/day/year hours:minutes
+    	SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy HH:mm a");
+    	File file = new File("C:/Users/Legato/Desktop/Workout Log.txt"); // File Location
+    	PrintWriter output = new PrintWriter(new FileWriter(file, true)); // Write to file without overwriting current contents
+    	output.println();
+		output.println(sdf.format(cal.getTime())); // Add date to file
+
+		JFrame frame = new Workout();
+		frame.setTitle("Luigi's Workout App");
 		JTextField workout1 = new JTextField("Push ups");
 		JTextField set1 = new JTextField();
 		JTextField rep1 = new JTextField();
-		add(workout1);
-		add(set1);
-		add(rep1);
+		frame.add(workout1);
+		frame.add(set1);
+		frame.add(rep1);
 
-		JTextField workout2 = new JTextField("Dumbell Press");
+		JTextField workout2 = new JTextField("Dumbell Presses");
 		JTextField set2 = new JTextField();
 		JTextField rep2 = new JTextField();
-		add(workout2);
-		add(set2);
-		add(rep2);
+		frame.add(workout2);
+		frame.add(set2);
+		frame.add(rep2);
 
 		JTextField workout3 = new JTextField("Dumbell Curls");
 		JTextField set3 = new JTextField();
 		JTextField rep3 = new JTextField();
-		add(workout3);
-		add(set3);
-		add(rep3);
+		frame.add(workout3);
+		frame.add(set3);
+		frame.add(rep3);
 
 		JCheckBox abs = new JCheckBox("Abs?");
 		JCheckBox squat = new JCheckBox("Squats?");
-		add(abs);
-		add(squat);
-	}
-
-	public static void main(String[] args) throws Exception {
-		JFrame frame = new Workout();
-		frame.setTitle("Luigi's Workout App");
-		JButton log = new JButton("Log");
-		// Get current date information
-		Calendar cal = Calendar.getInstance();
-		// Set format
-    	SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy HH:mm a");
-    	File file = new File("C:/Users/Legato/Desktop/Workout Log.txt");
-    	PrintWriter output = new PrintWriter(new FileWriter(file, true));
-		output.println(sdf.format(cal.getTime()));
-
+		frame.add(abs);
+		frame.add(squat);
+		JButton log = new JButton("Log"); // To log everything
+		// Register + create Event Listener
     	log.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("Closing...");
+				System.out.println("Comitting everything to file.\nClosing...");
+				output.println("\n" + set1.getText() + " x " + rep1.getText() + " " + workout1.getText());
+				output.println(set2.getText() + " x " + rep2.getText() + " " + workout2.getText());
+				output.println(set3.getText() + " x " + rep3.getText() + " " + workout3.getText());
 				output.close();
 			}
 		});
