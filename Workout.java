4ae38f7bf63a74a2dc2822b41dc7d8c2171f82cf
Luigi Vincent <luigivincenthr@gmail.com>
2014-06-29 12:23:57 -0400

    Update Workout.java

--- Workout.java
+++ Workout.java
@@ -1,5 +1,12 @@
 /* Author: Luigi Vincent
-A simple application to log workouts
+A simple application to keep track of workouts
+TO DO:
+ ** A way to check that set and rep are actually numbers - DONE
+ ** Not display them on the log if fields are left blank - DONE
+ ** Functionality to checkboxes
+ ** Consolidation of redundant information - DONE
+ ** Ensure file exists or create if it doesn't - DONE
+ ** Add analytics to keep track information e.g. weekly progres, progress since last workout, etc
 */
 
 import javax.swing.*;
@@ -24,36 +31,38 @@ public class Workout extends JFrame {
 		Calendar cal = Calendar.getInstance();
 		// Set format to month/day/year hours:minutes
     	SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy HH:mm a");
-    	File file = new File("C:/Users/Legato/Desktop/Workout Log.txt"); // File Location
+    	
+    	// Creates file object and sets pointer
+    	File file = new File("Workout Log.txt"); // File Location
+    	// Ensures file exists and is not a directory
+    	if ( !file.exists() || file.isDirectory() ) {
+			PrintWriter writer = new PrintWriter("Workout Log.txt", "UTF-8");
+			writer.close();
+		}
+
     	PrintWriter output = new PrintWriter(new FileWriter(file, true)); // Write to file without overwriting current contents
-    	output.println();
+    	output.println(); // Line break
 		output.println(sdf.format(cal.getTime())); // Add date to file
-
+		// Create Frame 
 		JFrame frame = new Workout();
-		frame.setTitle("Luigi's Workout Tracker");
-		ImageIcon appIcon = new ImageIcon("C:/MyWork/Images/Muscle.png");
+		frame.setTitle("Luigi's Workout Tracker"); // title
+		// Set Icon image
+		ImageIcon appIcon = new ImageIcon("Images/Muscle.png");
 		frame.setIconImage(appIcon.getImage());
-		
-		JTextField workout1 = new JTextField("Push ups");
-		JTextField set1 = new JTextField();
-		JTextField rep1 = new JTextField();
-		frame.add(workout1);
-		frame.add(set1);
-		frame.add(rep1);
 
-		JTextField workout2 = new JTextField("Dumbell Presses");
-		JTextField set2 = new JTextField();
-		JTextField rep2 = new JTextField();
-		frame.add(workout2);
-		frame.add(set2);
-		frame.add(rep2);
+		// workout, set, and rep fields for data
+		JTextField[] workouts = { new JTextField("Push ups"), new JTextField("Dumbell Presses"), new JTextField("Dumbell Curls") };
+		JTextField[] sets = new JTextField[3];
+		JTextField[] reps = new JTextField[3];
 
-		JTextField workout3 = new JTextField("Dumbell Curls");
-		JTextField set3 = new JTextField();
-		JTextField rep3 = new JTextField();
-		frame.add(workout3);
-		frame.add(set3);
-		frame.add(rep3);
+		// add textfield pointers + add textfields to frame
+		for (int i = 0; i < 3; i++) {
+			frame.add(workouts[i]);
+			sets[i] = new JTextField(); 
+			frame.add(sets[i]);
+			reps[i] = new JTextField();
+			frame.add(reps[i]);
+		}
 
 		JCheckBox abs = new JCheckBox("Abs?");
 		JCheckBox squat = new JCheckBox("Squats?");
@@ -65,12 +74,16 @@ public class Workout extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Comitting everything to file.\nClosing...");
-				output.println("\n" + set1.getText() + " x " + rep1.getText() + " " + workout1.getText());
-				output.println(set2.getText() + " x " + rep2.getText() + " " + workout2.getText());
-				output.println(set3.getText() + " x " + rep3.getText() + " " + workout3.getText());
+				output.println("\n"); // Line break
+				for (int i = 0; i < 3; i++) {
+					// Checks if fields are populated
+					if (sets[i].getText().length() > 0 && reps[i].getText().length() > 0) {
+						output.println( sets[i].getText() + " x " + reps[i].getText() + " " + workouts[i].getText() );
+					}
+				}
 				output.close();
 				try {
-				   Thread.sleep(1500);
+				   Thread.sleep(500);
 				} 
 				catch (InterruptedException i) {
 				   i.printStackTrace();
